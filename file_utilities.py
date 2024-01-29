f975c22b14686a4589a6186a41e45f6695919089
Ephemeral0121 <114738668+Ephemeral0121@users.noreply.github.com>
2024-01-29 23:37:21 +0900

    Update file_utilities.py

--- file_utilities.py
+++ file_utilities.py
@@ -4,15 +4,15 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QVBoxLayout, 
 from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtGui import QFont, QIcon
 
-# fimenaming.py와 image_resizer.py에서 클래스를 임포트한다고 가정합니다.
 from fimenaming import FileUploader
 from image_resizer2 import ImageResizer
+from Move_the_File import MovetheFile
 
 class MainApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("File Utilities")
-        self.setWindowIcon(QIcon("image_resizer_icon.png"))  # 애플리케이션 아이콘 설정 (적절한 경로로 대체 필요)
+        self.setWindowIcon(QIcon(self.resource_path("image_resizer_icon.png")))  
         self.resize(800, 600)
         self.initStartScreen()
 
@@ -26,24 +26,25 @@ class MainApp(QMainWindow):
     def initStartScreen(self):
         self.startWidget = QWidget()
         self.setCentralWidget(self.startWidget)
-
         layout = QVBoxLayout()
         layout.setAlignment(Qt.AlignCenter)
         layout.setSpacing(30)
-
         titleLabel = QLabel("Choose a Utility")
         titleLabel.setAlignment(Qt.AlignCenter)
         titleLabel.setFont(QFont("Arial", 28, QFont.Bold))
         layout.addWidget(titleLabel)
-        
 
-        self.fileRenamerButton = self.createButton("File Renamer", self.resource_path("file_renamer_icon.png"))  # 아이콘 파일 경로 추가
+        self.fileRenamerButton = self.createButton("File Renamer", self.resource_path("file_renamer_icon.png"))
         self.fileRenamerButton.clicked.connect(lambda: self.openTab(0))
         layout.addWidget(self.fileRenamerButton)
 
-        self.imageResizerButton = self.createButton("Image Resizer", self.resource_path("image_resizer_icon.png")) # 아이콘 파일 경로 추가
+        self.imageResizerButton = self.createButton("Image Resizer", self.resource_path("image_resizer_icon.png"))
         self.imageResizerButton.clicked.connect(lambda: self.openTab(1))
         layout.addWidget(self.imageResizerButton)
+
+        self.moveFileButton = self.createButton("Move the File", self.resource_path("move_the_file_icon.png")) 
+        self.moveFileButton.clicked.connect(lambda: self.openTab(2))
+        layout.addWidget(self.moveFileButton)
 
         self.startWidget.setLayout(layout)
 
@@ -60,16 +61,12 @@ class MainApp(QMainWindow):
 
     def initTabWidget(self):
         self.tab_widget = QTabWidget()
-        self.tab_widget.setStyleSheet("QTabBar::tab { height: 50px; }")  # 탭 세로 크기 조정
+        self.tab_widget.setStyleSheet("QTabBar::tab { height: 50px; }")
         self.setCentralWidget(self.tab_widget)
 
-        # 파일 리네이머 탭 초기화 및 아이콘 설정
-        fileRenamerTab = FileUploader()
-        self.tab_widget.addTab(fileRenamerTab, QIcon(self.resource_path("file_renamer_icon.png")), "File Renamer")  # 아이콘 추가
-
-        # 이미지 리사이저 탭 초기화 및 아이콘 설정
-        imageResizerTab = ImageResizer()
-        self.tab_widget.addTab(imageResizerTab, QIcon(self.resource_path("image_resizer_icon.png")), "Image Resizer")  # 아이콘 추가
+        self.tab_widget.addTab(FileUploader(), QIcon(self.resource_path("file_renamer_icon.png")), "File Renamer")
+        self.tab_widget.addTab(ImageResizer(), QIcon(self.resource_path("image_resizer_icon.png")), "Image Resizer")
+        self.tab_widget.addTab(MovetheFile(), QIcon(self.resource_path("move_the_file_icon.png")), "Move the File") 
 
     def openTab(self, index):
         self.initTabWidget()
